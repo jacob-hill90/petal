00cad8f1121b42cf824b4e3c1ecd4bae0972255b
jacob-hill90 <hilljake93@gmail.com>
2023-11-14 20:52:33 -0500

    working on card

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -2,16 +2,15 @@ import LogoPetal from "../../assets/petal.png";
 import "./style.css";
 
 export const Logo = ({ logoHeight }) => {
-  let link = window.location.origin;
-
   return (
     <div className="logo-wrapper">
-      <img
-        src={LogoPetal}
-        className="logo"
-        style={{ height: `${logoHeight}rem` }}
-        onClick={() => (window.location = link)}
-      ></img>
+      <a href="/">
+        <img
+          src={LogoPetal}
+          className="logo"
+          style={{ height: `${logoHeight}rem` }}
+        ></img>
+      </a>
     </div>
   );
 };
